906b364a9c6e859ef84cb16f3bba4188634f1e1f
flytreeleft <flytreeleft@crazydan.org>
2024-04-25 14:12:41 +0800

    Code/app-modeler: DevAppModule__importDiscovery 增加异常处理

--- code/app-modeler/biz/src/main/java/io/crazydan/duzhou/platform/app_modeler/biz/AppModelerBizErrors.java
+++ code/app-modeler/biz/src/main/java/io/crazydan/duzhou/platform/app_modeler/biz/AppModelerBizErrors.java
@@ -1,6 +1,12 @@
 package io.crazydan.duzhou.platform.app_modeler.biz;
 
+import io.nop.api.core.exceptions.ErrorCode;
+
+import static io.nop.api.core.exceptions.ErrorCode.define;
+
 /** 业务{@link io.nop.api.core.exceptions.ErrorCode 错误信息}定义 */
 public interface AppModelerBizErrors {
 
+    ErrorCode Err_DevAppModule_No_App_Exist = define("nop.err.biz.DevAppModule.no-app-exist",
+                                                     "未指定所属应用或指定的应用不存在");
 }
